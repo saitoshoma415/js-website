b3cce5115985aee776527d68ec1c6015b9de5a83
斉藤翔馬 <s.shoma415@icloud.com>
2022-07-26 00:08:59 +0900

    js-website

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-app.js;
+var circle = document.getElementById("circle");
+      var upBtn = document.getElementById("upBtn");
+      var downBtn = document.getElementById("downBtn");
+
+      var rotateValue = circle.style.transform;
+      var rotateSum;
+
+      upBtn.onclick = function () {
+        /* 時計回りに90°回転させる */
+        rotateSum = rotateValue + "rotate(-90deg)";
+        circle.style.transform = rotateSum;
+        rotateValue = rotateSum;
+      };
+      downBtn.onclick = function () {
+        /* 時計回りに90°回転させる */
+        rotateSum = rotateValue + "rotate(90deg)";
+        circle.style.transform = rotateSum;
+        rotateValue = rotateSum;
+      };
